508e48a34b56b2beebda27c3304373c861f7a96c
Aditi Jain <anjain@usc.edu>
2025-02-01 06:23:46 +0000

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -4,10 +4,47 @@
 #include <sstream>
 
 #include "ulliststr.h"
+using namespace std;
 
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
+    ULListStr ullist = ULListStr();
+    // tesing push_back
+    ullist.push_back("4");
+    ullist.push_back("5");
+    ullist.push_back("6");
+    // 4,5,6
+    cout << "testing push_back...\n";
+    for (size_t i = 0; i < ullist.size(); i++) {
+        cout << ullist.get(i) << endl;
+    }
+    // tesing push_front
+    ullist.push_front("3");
+    ullist.push_front("2");
+    ullist.push_front("1");
 
+    cout << "\ntesing push_front...\n";
+    for (size_t i = 0; i < ullist.size(); i++) {
+        cout << ullist.get(i) << endl;
+    }
+
+    ullist.pop_front();
+    cout << "\ntesting pop_front...\n";
+    for (size_t i = 0; i < ullist.size(); i++) {
+        cout << ullist.get(i) << endl;
+    }
+
+    ullist.pop_back();
+    cout << "\ntesting pop_back...\n";
+    for (size_t i = 0; i < ullist.size(); i++) {
+        cout << ullist.get(i) << endl;
+    }
+
+    cout << "\ntesting front...\n";
+    cout << ullist.front();
+
+    cout << "\ntesting back...\n";
+    cout << ullist.back();
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 #include "ulliststr.h"
 
+using namespace std;
 ULListStr::ULListStr()
 {
   head_ = NULL;
@@ -24,7 +25,130 @@ size_t ULListStr::size() const
   return size_;
 }
 
-// WRITE YOUR CODE HERE
+/**
+   * Adds a new value to the back of the list
+   *   - MUST RUN in O(1)
+   */
+void ULListStr::push_back(const std::string& val) {
+    if (empty()) {
+        // Create first item if list is empty
+        Item* newItem = new Item();
+        head_ = tail_ = newItem;
+        tail_->first = 0;
+        tail_->last = 0;
+    }
+    else if (tail_->last >= ARRSIZE) {
+        // Create new item if current tail is full
+        Item* newItem = new Item();
+        newItem->prev = tail_;
+        tail_->next = newItem;
+        tail_ = newItem;
+        tail_->first = 0;
+        tail_->last = 0;
+    }
+    
+    // Add the new value
+    tail_->val[tail_->last] = val;
+    tail_->last++;
+    size_++;
+}
+
+/**
+  * Removes a value from the back of the list
+  *   - MUST RUN in O(1)
+  */
+void ULListStr::pop_back() {
+  if (!empty()) {
+      tail_->last--;
+      size_--;
+
+      if (tail_->last == tail_->first) { // The item is now empty
+          Item* temp = tail_;
+          tail_ = tail_->prev;
+
+          if (tail_ != NULL) {
+              tail_->next = NULL;
+          } else {
+              head_ = NULL;  // both head and tail are NULL when the list is empty
+          }
+
+          delete temp;
+      }
+  }
+}
+
+/**
+  * Adds a new value to the front of the list.
+  * If there is room before the 'first' value in
+  * the head node add it there, otherwise, 
+  * allocate a new head node.
+  *   - MUST RUN in O(1)
+*/
+void ULListStr::push_front(const std::string& val) {
+  if (empty() || head_->first == 0) {
+      Item* newItem = new Item();
+      newItem->next = head_;
+      if (head_ != NULL) {
+          head_->prev = newItem;
+      } else {
+          tail_ = newItem;
+      }
+      head_ = newItem;
+      newItem->first = ARRSIZE;
+      newItem->last = ARRSIZE;
+  } 
+  head_->first--;
+  head_->val[head_->first] = val;
+  size_++;
+}
+
+  /**
+   * Removes a value from the front of the list
+   *   - MUST RUN in O(1)
+   */
+void ULListStr::pop_front() {
+    if (!empty()) {
+        head_->first++;
+        size_--;
+
+        if (head_->first == head_->last) { // The item is now empty
+            Item* temp = head_;
+            
+            head_ = head_->next;
+
+            if (head_ != NULL) {
+                head_->prev = NULL;
+            } else {
+                tail_ = NULL;  // both head and tail are NULL when the list is empty
+            }
+
+            delete temp;
+            
+        }
+    }
+}
+  
+  /**
+   * Returns a const reference to the back element
+   *   - MUST RUN in O(1)
+   */
+  std::string const & ULListStr::back() const {
+    if (empty()) {
+        throw std::out_of_range("List is empty");
+    }
+    return tail_->val[tail_->last - 1];
+  }
+
+  /**
+   * Returns a const reference to the front element
+   *   - MUST RUN in O(1)
+   */
+std::string const & ULListStr::front() const {
+    if (empty()) {
+        throw std::out_of_range("List is empty");
+    }
+    return head_->val[head_->first];
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
@@ -55,11 +179,31 @@ std::string const & ULListStr::get(size_t loc) const
 
 void ULListStr::clear()
 {
-  while(head_ != NULL){
-    Item *temp = head_->next;
+  Item *temp = head_;
+  while(head_ != NULL) {
+    temp = head_->next;
     delete head_;
     head_ = temp;
   }
   tail_ = NULL;
   size_ = 0;
+}
+
+/** 
+   * Returns a pointer to the item at index, loc,
+   *  if loc is valid and NULL otherwise
+   *   - MUST RUN in O(n) 
+   */
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+    size_t index = 0;
+    Item* temp = head_;
+
+    while (temp != NULL) {
+        size_t numElements = temp->last - temp->first;
+        if (loc < index + numElements) return &temp->val[temp->first + (loc - index)];
+        index += numElements;
+        temp = temp->next;
+       
+    }
+  return NULL;
 }
